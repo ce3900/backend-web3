e1ae4fe0ea2a56ef27e69635acf142c4a62a94db
andyc00l404 <160966653+andycool202@users.noreply.github.com>
2024-06-27 06:19:54 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import './index.js';
+import { FaShareSquare, FaMoneyBillWave, FaBroadcastTower } from 'react-icons/fa';
+import './index.css';
 
 function App() {
   const [userId, setUserId] = useState(null);
@@ -15,7 +16,7 @@ function App() {
         }
         if (initDataUnsafe.user) {
           setUserId(initDataUnsafe.user.id);
-          setMessage(`Tu IDda de Telegram es: ${initDataUnsafe.user.id}`);
+          setMessage(`Tu ID de Telegram es: ${initDataUnsafe.user.id}`);
         } else {
           setMessage('Usuario no identificado o datos iniciales no disponibles');
         }
@@ -28,8 +29,8 @@ function App() {
   }, []);
 
   return (
-    <div className="bg-dark-black text-white min-h-screen flex flex-col items-center justify-center p-4">
-      <div className="w-full max-w-sm bg-gray-800 p-6 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md bg-gray-800 p-6 rounded-lg shadow-lg text-center">
         <h1 className="text-2xl font-bold mb-2">WEB APPS</h1>
         <p className="mb-4">{message}</p>
         <div className="bg-gray-700 p-4 rounded-lg shadow">
@@ -38,9 +39,15 @@ function App() {
           <img src="minero1.jpg" alt="Mining Plan" className="mx-auto w-32 h-32 rounded-full mt-3" />
         </div>
         <div className="grid grid-cols-3 gap-4 mt-4">
-          <button className="bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Share</button>
-          <button className="bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Withdraw</button>
-          <button className="bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Live Payments</button>
+          <button className="flex justify-center items-center bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <FaShareSquare className="mr-2" /> Share
+          </button>
+          <button className="flex justify-center items-center bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <FaMoneyBillWave className="mr-2" /> Withdraw
+          </button>
+          <button className="flex justify-center items-center bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <FaBroadcastTower className="mr-2" /> Live Payments
+          </button>
         </div>
         <div className="text-left mt-4">
           <h2 className="text-lg font-bold">FAQ</h2>
