abc9b5a1fda5ee2b7b015bfaf14d87d95f0cc4d6
andyc00l404 <160966653+andycool202@users.noreply.github.com>
2024-06-27 04:20:28 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         }
         if (initDataUnsafe.user) {
           setUserId(initDataUnsafe.user.id);
-          setMessage(`Tu ID de Telegram es: ${initDataUnsafe.user.id}`);
+          setMessage(`Tu IDd de Telegram es: ${initDataUnsafe.user.id}`);
         } else {
           setMessage('Usuario no identificado o datos iniciales no disponibles');
         }
@@ -28,18 +28,20 @@ function App() {
 
   return (
     <div className="bg-dark-black text-white min-h-screen flex flex-col items-center justify-center p-4">
-      <div className="space-y-4 w-full max-w-sm bg-gray-800 p-6 rounded-lg shadow-lg text-center">
-        <h1 className="text-2xl font-bold">WEB APPS</h1>
-        <p className="text-gray-300">{message}</p>
-        {startAppParam && <p>Valor del parámetro startapp: {startAppParam}</p>}
-        {userId && <p>Tu ID de Telegram es: {userId}</p>}
-        <img src="minero1.jpg" alt="Mining Plan" className="mx-auto w-full py-4" />
-        <div className="grid grid-cols-2 gap-3">
+      <div className="w-full max-w-sm bg-gray-800 p-6 rounded-lg shadow-lg text-center">
+        <h1 className="text-2xl font-bold mb-2">WEB APPS</h1>
+        <p className="mb-4">{message}</p>
+        <div className="bg-gray-700 p-4 rounded-lg shadow">
+          {userId && <p>Tu ID de Telegram es: {userId}</p>}
+          {startAppParam && <p>Valor del parámetro startapp: {startAppParam}</p>}
+          <img src="minero1.jpg" alt="Mining Plan" className="mx-auto w-32 h-32 rounded-full mt-3" />
+        </div>
+        <div className="grid grid-cols-3 gap-4 mt-4">
           <button className="bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Share</button>
           <button className="bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Withdraw</button>
+          <button className="bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Live Payments</button>
         </div>
-        <button className="bg-money-green hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full mt-3">Live Payments</button>
-        <div className="pt-4">
+        <div className="text-left mt-4">
           <h2 className="text-lg font-bold">FAQ</h2>
           <p>How can I withdraw my balance?</p>
           <p>When will my withdrawal be processed?</p>
